ba5b50b2f09e24b0ca4de476dd7455e969fae6c0
juanf <jfruizg@unbosque.edu.co>
2021-05-12 14:37:17 -0500

    Initial commit

---
+++ src/main/java/edu/unbosque/JPATutorial/services/BookService.java
@@ -0,0 +1,76 @@
+package edu.unbosque.JPATutorial.services;
+
+import edu.unbosque.JPATutorial.jpa.entities.Author;
+import edu.unbosque.JPATutorial.jpa.entities.Book;
+import edu.unbosque.JPATutorial.jpa.entities.Edition;
+import edu.unbosque.JPATutorial.jpa.repositories.AuthorRepository;
+import edu.unbosque.JPATutorial.jpa.repositories.AuthorRepositoryImpl;
+import edu.unbosque.JPATutorial.jpa.repositories.BookRepository;
+import edu.unbosque.JPATutorial.jpa.repositories.BookRepositoryImpl;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Optional;
+
+@Stateless
+public class BookService {
+
+    AuthorRepository authorRepository;
+    BookRepository bookRepository;
+
+    public void saveBook(String title, String isbn, Integer authorId) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tutorial");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        authorRepository = new AuthorRepositoryImpl(entityManager);
+        bookRepository = new BookRepositoryImpl(entityManager);
+
+        Optional<Author> author = authorRepository.findById(authorId);
+
+
+
+        author.ifPresent(a -> {
+            a.addBook(new Book(title, isbn));
+            authorRepository.save(a);
+        });
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return;
+
+    }
+    public void addEdicion(Integer isbn, Edition edicion){
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tutorial");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        authorRepository = new AuthorRepositoryImpl(entityManager);
+        bookRepository = new BookRepositoryImpl(entityManager);
+
+        Optional<Book> author = bookRepository.findById(isbn);
+
+        author.ifPresent(a -> {
+            a.addEdition((edicion));
+            bookRepository.save(a);
+        });
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return;
+
+
+
+    }
+    public void deleteBookAuthor(){
+
+
+
+
+    }
+
+}
